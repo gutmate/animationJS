a2aced87a8647e372cdfc5c0c10aee20cf31d097
jinh <jinh.ppp@gmail.com>
2017-03-10 14:43:14 +0900

    html 테스트 수정

--- js/animationjs-1.0.js
+++ js/animationjs-1.0.js
@@ -11,55 +11,58 @@
  * @delay {number} 애니메이션 시작 대기시간
  */
 ;(function($){
-    $.fn.animationImg = function(steps, duration, repeat, delay) {
-        var $this = $(this);
-        var path = $this.attr('src');
-        var rePath = /.+(?=[0-9]{4}.)/gm;
-        var matchPath = path.match(rePath); //4자리 숫자.확장자를 제외한 경로부분 반환
-        var reversePath = path.replace(rePath,''); //4자리 숫자.확장자 반환
-        matchPath = matchPath || ['null']; //에러 방지
-        var imgPath = matchPath[0]; //배열에서 값 가져오기
-        var reExtn = /[0-9]+/gm; //모든 숫자 선택
-        var imgExtn = reversePath.replace(reExtn,''); //4자리숫자.확장자 값에서 확장자만 반환
-        var startNum = 0;
-        var arrImg = [];
-        steps = steps || 0;
-        duration = duration || 100;
-        repeat = repeat || false;
-        delay = delay || 0;
 
-        for(i=startNum; i<steps+1; i++) {
-            if (i < 10) {
-                arrImg.push(imgPath + '000' + i + imgExtn);
-            } else if (i < 100) {
-                arrImg.push(imgPath + '00' + i + imgExtn);
-            } else if (i < 1000) {
-                arrImg.push(imgPath + '0' + i + imgExtn);
+  $.fn.animationImg = function(steps, duration, repeat, delay) {
+    var $this = $(this);
+    var path = $this.attr('src');
+    var rePath = /.+(?=[0-9]{4}.)/gm;
+    var matchPath = path.match(rePath); //4자리 숫자.확장자를 제외한 경로부분 반환
+    var reversePath = path.replace(rePath,''); //4자리 숫자.확장자 반환
+    matchPath = matchPath || ['null']; //에러 방지
+    var imgPath = matchPath[0]; //배열에서 값 가져오기
+    var reExtn = /[0-9]+/gm; //모든 숫자 선택
+    var imgExtn = reversePath.replace(reExtn,''); //4자리숫자.확장자 값에서 확장자만 반환
+    var startNum = 0;
+    var arrImg = [];
+    
+    steps = steps || 0;
+    duration = duration || 100;
+    repeat = repeat || false;
+    delay = delay || 0;
+
+    for(i=startNum; i<steps+1; i++) {
+      if (i < 10) {
+        arrImg.push(imgPath + '000' + i + imgExtn);
+      } else if (i < 100) {
+        arrImg.push(imgPath + '00' + i + imgExtn);
+      } else if (i < 1000) {
+        arrImg.push(imgPath + '0' + i + imgExtn);
+      } else {
+        arrImg.push(imgPath + i + imgExtn);
+      }
+    }
+
+    //올바른 값일때만 실행
+    if (matchPath[0] !== 'null') {
+      setTimeout(function(){
+        var anim = setInterval(aniImg, duration);
+
+        function aniImg() {
+          startNum = startNum + 1;
+          if (startNum > steps) {
+            if (repeat === 'repeat') {
+              startNum = 0; //반복
             } else {
-                arrImg.push(imgPath + i + imgExtn);
+              clearInterval(anim); //정지
             }
+          } else {
+              $this.attr('src',arrImg[startNum]);
+          }
         }
+      }, delay);
+    } else {
+      return false;
+    }
+  };
 
-        //올바른 값일때만 실행
-        if (matchPath[0] !== 'null') {
-            setTimeout(function(){
-                var anim = setInterval(aniImg, duration);
-
-                function aniImg() {
-                    startNum = startNum + 1;
-                    if (startNum > steps) {
-                      if (repeat === 'repeat') {
-                        startNum = 0; //반복
-                      } else {
-                        clearInterval(anim); //정지
-                      }
-                    } else {
-                        $this.attr('src',arrImg[startNum]);
-                    }
-                }
-            }, delay);
-        } else {
-            return false;
-        }
-    };
 })(jQuery);
