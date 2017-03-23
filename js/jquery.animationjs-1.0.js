0efd7e6ed2dba6157340a84d1d205039261e2b9d
jinh_gangil <gutmate@gmail.com>
2017-03-24 00:54:47 +0900

    Add default settings

--- js/jquery.animationjs-1.0.js
+++ js/jquery.animationjs-1.0.js
@@ -6,7 +6,7 @@
  */
 ;(function($){
 
-  $.fn.animationImg = function(steps, duration, repeat, delay) {
+  $.fn.animationImg = function(settings) {
     var $this = $(this);
     var path = $this.attr('src');
     var rePath = /.+(?=[0-9]{4}.)/gm;
@@ -16,15 +16,18 @@
     var imgPath = matchPath[0]; //배열에서 값 가져오기
     var reExtn = /[0-9]+/gm; //모든 숫자 선택
     var imgExtn = reversePath.replace(reExtn,''); //4자리숫자.확장자 값에서 확장자만 반환
-    var startNum = 0;
     var arrImg = [];
-    
-    steps = steps || 0;
-    duration = duration || 100;
-    repeat = repeat || false;
-    delay = delay || 0;
 
-    for(i=startNum; i<steps+1; i++) {
+    //기본값 설정 & 사용자 설정과 병합
+    var option = $.extend({
+      startNum: 0,
+      steps: 10,
+      duration: 30,
+      repeat: true,
+      delay: 0
+    }, settings );
+
+    for(i=option.startNum; i<option.steps+1; i++) {
       if (i < 10) {
         arrImg.push(imgPath + '000' + i + imgExtn);
       } else if (i < 100) {
@@ -39,24 +42,32 @@
     //올바른 값일때만 실행
     if (matchPath[0] !== 'null') {
       setTimeout(function(){
-        var anim = setInterval(aniImg, duration);
+        var anim = setInterval(aniImg, option.duration);
 
         function aniImg() {
-          startNum = startNum + 1;
-          if (startNum > steps) {
-            if (repeat === 'repeat') {
-              startNum = 0; //반복
+          option.startNum = option.startNum + 1;
+          if (option.startNum > option.steps) {
+            if (option.repeat) {
+              option.startNum = 0; //반복
             } else {
               clearInterval(anim); //정지
             }
           } else {
-              $this.attr('src',arrImg[startNum]);
+              $this.attr('src',arrImg[option.startNum]);
           }
         }
-      }, delay);
+      }, option.delay);
     } else {
       return false;
     }
+
+    /**
+     * 개선해야 할 과제
+     *
+     * 이미지가 없어서 에러가 날 때에 에러난 시점을 저장해서 다음 반복 실행시에는
+     * 종료 시점을 에러나기 전 시점으로 재실행시켜주기
+     * 그러면 두번째 반복부터는 에러가 나지 않을 것이니까
+     */
   };
 
 })(jQuery);
